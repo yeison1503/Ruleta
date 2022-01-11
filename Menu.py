f2c2988d650a3c73cd5e84eeb91553423a5b0fd8
Yeison Monsalve <81596987+yeison1503@users.noreply.github.com>
2022-01-11 04:18:33 -0500

    Se agrego al menú inicial algunas funciones para el CRUD

--- Menu.py
+++ Menu.py
@@ -1,5 +1,41 @@
 import funciones
 
+def Ingresar_jugador():
+    nombre = str(input("Ingrese su nombre.\n"))
+    # if nombre == funciones.consultar(nombre):
+    #     print("El nombre ingresado ya se encuentra en uso.\n")
+    # else:
+    edad = input("Ingrese la edad.\n")
+    dinero = input("Ingrese el valor del dinero.\n")
+    funciones.NEW_jugador(nombre, edad, dinero)
+    print("Jugador Creado con Exito!")
+
+def Eli_Mod_jugador():
+    print ("""
+    1.Modificar Jugador.
+    2.Eliminar Jugador.
+    3.Atras.
+    """)
+    opt=input("Elige: \n") 
+    #Modificar jugador
+    if opt=="1": 
+        print("Se debe ingresar el id para poder modificar los datos del usuario.\n")
+        id = str(input("Ingrese el id del jugador que desea modificar.\n"))
+        nombre = input("Ingrese el nuevo nombre.\n")
+        edad = input("Ingrese la nueva edad.\n")
+        dinero = input("Ingrese el nuevo valor del dinero.\n")
+        funciones.modificar(nombre, edad, dinero, id)
+        print("Jugador Modificado con Exito!.")
+    #Eliminar jugador
+    elif opt=="2":
+        nombre = str(input("Ingrese el nombre del jugador que desea eliminar\n"))
+        funciones.eliminar(nombre)
+        print("Jugador Eliminado  correctamente!\n")
+    elif opt=="3":
+        pass
+    elif opt !="1" or opt !="2" or opt !="3" or opt !=" " :
+      print("\n Elección no válida!, Inténtelo de nuevo\n")
+
 
 #menu de Inicio
 print('*' * 50)
@@ -17,17 +53,13 @@ while ans:
     """)
     ans=input("¿Qué te gustaría hacer? \n") 
     if ans=="1": 
-        nombre = str(input("Ingrese su nombre.\n"))
-        edad = input("Ingrese la edad.\n")
-        dinero = input("Ingrese el valor del dinero.\n")
-        funciones.NEW_jugador(nombre, edad, dinero)
-        print("Jugador Creado con Exito!")
+        Ingresar_jugador()
     elif ans=="2":
-        RULETA()
+        print("RULETA()")
     elif ans=="3":
-        Delete_jugador()
+        Eli_Mod_jugador()
     elif ans=="4":
-        Listar_jugador()
+        funciones.Listar_jugador()
     elif ans=="5":
         print("\n Goodbye") 
         ans = False
--- funciones.py
+++ funciones.py
@@ -3,7 +3,45 @@ from conexion import obtener_conexion
 def NEW_jugador(nombre, edad, dinero):
     conexion = obtener_conexion()
     with conexion.cursor() as cursor:
-        cursor.execute("INSERT INTO juegos(nombre, descripcion, precio) VALUES (%s, %s, %s)",
+        cursor.execute("INSERT INTO jugadores(name, age, cash) VALUES (%s, %s, %s);",
                        (nombre, edad, dinero))
     conexion.commit()
     conexion.close()
+
+def consultar(nombre):
+    conexion = obtener_conexion()
+    with conexion.cursor() as cursor:
+        consulta = ("SELECT name FROM jugadores WHERE name = %s;",(nombre))
+        #nom = cursor.execute(consulta, nombre)
+        nom = cursor.fetchone()
+    conexion.close()
+    return nombre
+
+def eliminar(nombre):
+    conexion = obtener_conexion()
+    with conexion.cursor() as cursor:
+        cursor.execute("DELETE FROM jugadores WHERE name = %s;",(nombre))
+    conexion.commit()
+    conexion.close()
+
+def modificar(nombre, edad, dinero, id):
+    conexion = obtener_conexion()
+    with conexion.cursor() as cursor:
+        consulta = "UPDATE jugadores SET name = %s, age = %s, cash = %s WHERE id = %s;"
+        cursor.execute(consulta, (nombre, edad, dinero, id))
+        # cursor.execute("UPDATE jugadores SET name = %s, age = %s, cash = %s WHERE id = %s",
+        #                (nombre, edad, dinero, id))
+    conexion.commit()
+    conexion.close()
+    
+def Listar_jugador():
+    conexion = obtener_conexion()
+    jugadores = []
+    with conexion.cursor() as cursor:
+        cursor.execute("SELECT id, name, age, cash FROM jugadores;")
+        jugadores = cursor.fetchall()
+		
+        for jugadores in jugadores:
+            print(jugadores)
+    conexion.close()
+    return jugadores
